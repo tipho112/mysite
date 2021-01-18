5b86475cd9570041290a9fa7982712395d338963
bit <bit@DESKTOP-8KNVLKJ>
2021-01-18 16:00:49 +0900

    MySite06 - Spring Boot #2

--- mysite06/src/main/java/com/bitacademy/mysite/config/WebConfig.java
+++ mysite06/src/main/java/com/bitacademy/mysite/config/WebConfig.java
@@ -1,17 +1,78 @@
 package com.bitacademy.mysite.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
+import java.util.List;
 
-import com.bitacademy.config.web.MessageSourceConfig;
-import com.bitacademy.config.web.MvcConfig;
-import com.bitacademy.config.web.SecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.bitacademy.security.AuthInterceptor;
+import com.bitacademy.security.AuthUserHandlerMethodArgumentResolver;
+import com.bitacademy.security.LoginInterceptor;
+import com.bitacademy.security.LogoutInterceptor;
 
 @Configuration
-@EnableAspectJAutoProxy
-@ComponentScan({"com.bitacademy.mysite.controller", "com.bitacademy.mysite.exception"})
-@Import({MvcConfig.class, SecurityConfig.class, MessageSourceConfig.class})
-public class WebConfig {
+@PropertySource("classpath:config/webconfig.properties")
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Autowired
+	private Environment env;
+	
+	// Argument Resolvers
+	@Bean
+	public HandlerMethodArgumentResolver authUserHandlerMethodArgumentResolver() {
+		return new AuthUserHandlerMethodArgumentResolver();
+	}
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		argumentResolvers.add(authUserHandlerMethodArgumentResolver());
+	}
+	
+	// Interceptors
+	@Bean
+	public HandlerInterceptor loginInterceptor() {
+		return new LoginInterceptor();
+	}
+	
+	@Bean
+	public HandlerInterceptor logoutInterceptor() {
+		return new LogoutInterceptor();
+	}
+	
+	@Bean
+	public HandlerInterceptor authInterceptor() {
+		return new AuthInterceptor();
+	}
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry
+			.addInterceptor(loginInterceptor())
+			.addPathPatterns(env.getProperty("web.auth-url"));
+		
+		registry
+			.addInterceptor(logoutInterceptor())
+			.addPathPatterns(env.getProperty("web.logout-url"));
+		
+		registry
+			.addInterceptor(authInterceptor())
+			.addPathPatterns("/**")
+			.excludePathPatterns(env.getProperty("web.auth-url"))
+			.excludePathPatterns(env.getProperty("web.logout-url"))
+			.excludePathPatterns(env.getProperty("web.statics-url") + "/**");
+	}
+	
+	// MVC Resources Mapping(URL Magic Mapping)
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+			.addResourceHandler(env.getProperty("resource.mapping"))
+			.addResourceLocations("file:" + env.getProperty("resource.locations"));
+	}
 }
